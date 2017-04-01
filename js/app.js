951531202c472496892eeac314bfdb3d1d781beb
vasana1 <vasana1@yahoo.com>
2017-03-31 17:53:32 -1000

    finished

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 //Secret Menu Challenge
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var showMenu = document.getElementsByClassName("name");
 
+console.log(showMenu);
+
+for (var i=0; i<showMenu.length; i++){
+  console.log(showMenu[i]);
+  showMenu[i].addEventListener("click", secretMenu);
+}
+
+function secretMenu(){
+  var toggle = this.querySelectorAll(".menu")[0];
+  if (toggle.style.display === "none"){
+    toggle.style.display = "block";
+  }else{
+    toggle.style.display = "none";
+  }
+}
